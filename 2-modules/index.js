19eded1b1d42639e634baed08bc94a96ac957432
Muhammad Sameem <sameemgujjar123@yahoo.com>
2025-07-15 00:48:39 +0400

    ✔️ reorganized the files and add module part

---
+++ 2-modules/index.js
@@ -0,0 +1,7 @@
+const add = require("./add");
+
+console.log("This is index js file");
+
+const sum = add(7, 8);
+
+console.log(sum);
---
+++ 2-modules/module-scope.js
@@ -0,0 +1,13 @@
+// iife - Immediately Invoked Function Expression
+
+// function 1
+(function () {
+  const superhero = "Batman";
+  console.log(superhero);
+})();
+
+// function 2
+(function () {
+  const superhero = "Superman";
+  console.log(superhero);
+})();
